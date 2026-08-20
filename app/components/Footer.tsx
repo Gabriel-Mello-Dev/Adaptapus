cbff5ca7b2bc56cd17a7509c4135e4b581694791
CA1804 <ca18.04.2024@gmail.com>
2026-08-19 23:08:18 -0300

    [style] Trocados os textos provisórios do Footer e o Título principal da página do App

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,12 @@
 export default function Footer() {
 
     return(
-        <footer className="w-full bg-purpleMain p-4 min-h-1/6">
-            <p className="text-2xl text-greenSecond justify-between items-center">
-                ABC
+        <footer className="w-full bg-purpleMain p-4 min-h-1/6 flex justify-between items-center">
+            <p className="text-xl text-greenSecond">
+                Adaptil™
+            </p>
+            <p className="text-xl text-greenSecond">
+                @Copyright - 2026 - Todos os direitos reservados
             </p>
         </footer>
     )

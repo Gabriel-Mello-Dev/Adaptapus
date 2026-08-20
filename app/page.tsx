b4b3b16d46124c16c0d2a99c84075f13863f7dad
CA1804 <ca18.04.2024@gmail.com>
2026-08-19 23:01:48 -0300

    [style] Página inicial reestilizada

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,31 @@ export default function Home() {
   return (
     <div className="min-w-full min-h-full bg-whiteMain">
       <Header />
-      <main className="min-h-160 flex items-center justify-center p-6">
-        <div className="w-full max-w-md bg-white/10 backdrop-blur-md border border-white/10 rounded-3xl p-8 shadow-2xl">
-          <h1 className="text-4xl font-bold text-white text-center mb-8">
+      <main className="min-h-160 flex  flex-col items-center justify-center p-6">
+          <h1 className="text-4xl font-bold text-greenMain text-center mb-8">
             Adaptil- Menu Principal
           </h1>
+          <h2 className="text-2xl font-bold text-greenMain text-center mb-8">
+            O que vamos fazer hoje?
+          </h2>
 
-          <div className="flex flex-col gap-5">
-            <Link href="/pages/Room">
-              <button className="w-full py-4 rounded-2xl bg-purple-500 hover:bg-purple-400 transition-all duration-200 text-white text-lg font-semibold shadow-lg hover:scale-[1.02] active:scale-[0.98]">
-                Chat ao vivo (em desenvolvimento)
-              </button>
-            </Link>
-
-            <Link href="/pages/Adaptar">
-              <button className="w-full py-4 rounded-2xl bg-violet-600 hover:bg-violet-500 transition-all duration-200 text-white text-lg font-semibold shadow-lg hover:scale-[1.02] active:scale-[0.98]">
+          <div className="grid grid-cols-2 gap-5 items-center justify-center">
+            <Link href="/pages/Adaptar" className="col-span-2 justify-center">
+              <button className="bg-orangeMain text-white p-4 text-xl w-full rounded-3xl">
                 Adaptar Questões
-              </button>
+              </button> 
+            </Link>
+            <Link href="/pages/Room">
+              <button className="bg-orangeMain text-white p-4 text-xl w-96 rounded-3xl">
+                Entrar na Sala
+              </button> 
+            </Link>
+            <Link href="/pages/Room">
+              <button className="bg-orangeMain text-white p-4 text-xl w-96 rounded-3xl">
+                Criar Sala
+              </button> 
             </Link>
           </div>
-        </div>
       </main>
       <Footer />
     </div>

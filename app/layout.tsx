4c8f4170d46a014b7fb60325b37b47b80e440efd
cFischer08 <c.fischer08.11.2008@gmail.com>
2026-08-29 15:40:33 -0300

    [style] Removidas as alterações visuais causadas pelo shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Adaptapus",
-  description: "Adaptador de questões didaticas para temas de seu interesse utilizando Inteligencia artificial",
+  description:
+    "Adaptador de questões didaticas para temas de seu interesse utilizando Inteligencia artificial",
 };
 
 export default function RootLayout({
@@ -24,12 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-
-        <Analytics/>
+    <html lang="pt-BR">
+      <body>
+        <Analytics />
         {children}
       </body>
     </html>

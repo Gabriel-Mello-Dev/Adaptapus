525d68639ae40ea230c1f02545cfd678895741a4
cFischer08 <c.fischer08.11.2008@gmail.com>
2026-09-09 21:39:23 -0300

    [feat] Botão de voltar consertado.

--- app/components/BackButton.tsx
+++ app/components/BackButton.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import Link  from "next/link";
 import { ArrowLeft } from "lucide-react";
 
 export default function BackButton() {
-  const router = useRouter();
 
   return (
-    <button
-      type="button"
-      onClick={() => router.back()}
+    <Link
+      href="/"
       className="rounded-lg px-4 py-2 bg-gray-800 text-white hover:bg-gray-700 transition"
     >
       < ArrowLeft className="inline-block mr-2" size={16} />
       Voltar
-    </button>
+    </Link>
   );
 }

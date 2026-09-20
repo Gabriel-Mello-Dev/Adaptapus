a9fbc93ee01fb892510441477b556470ab857cd2
cFischer08 <c.fischer08.11.2008@gmail.com>
2026-09-20 19:55:59 -0300

    [style] Botão de voltar adaptado para mobile

--- app/components/BackButton.tsx
+++ app/components/BackButton.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import Link  from "next/link";
+import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 
 export default function BackButton() {
-
   return (
     <Link
       href="/"
-      className="rounded-lg px-4 py-2 bg-gray-800 text-white hover:bg-gray-700 transition h-16 w-28 text-md flex items-center justify-center gap-1"
+      className="
+        rounded-lg
+        bg-gray-800
+        text-white
+        hover:bg-gray-700
+        transition
+        h-10
+        w-20
+        text-sm
+        flex
+        items-center
+        justify-center
+        gap-1
+        md:h-16
+        md:w-28
+        md:text-md
+      "
     >
-      < ArrowLeft className="inline-block mr-2" size={16} />
+      <ArrowLeft
+        className="inline-block mr-1 md:mr-2"
+        size={16}
+      />
       Voltar
     </Link>
   );
